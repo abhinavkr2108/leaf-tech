1f1f6a9dcc34be1080336ec32bea1dd09c0a6152
Abhinav Kumar <abhinavkr.2108@gmail.com>
2024-05-17 23:35:42 +0530

    Fixed route.ts

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,10 +1,9 @@
 import { PrismaClient } from "@prisma/client";
-import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function GET(req: NextApiRequest, res: Response) {
+export async function GET() {
   try {
     const tasks = await prisma.task.findMany();
     console.log(tasks);
